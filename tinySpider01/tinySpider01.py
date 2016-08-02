0590ac08f436229d77fa8f3d2251d0a70af152e5
SeptemberHX <september.hx@gmail.com>
2016-08-02 16:17:05 +0800

    add tinySpider01.py

---
+++ tinySpider01/tinySpider01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 8/2/16 3:32 PM
+# @Author  : jiuyue
+# @Site    : 
+# @File    : tinySpider01.py.py
+# @Software: PyCharm
+# @Version : 1.0
